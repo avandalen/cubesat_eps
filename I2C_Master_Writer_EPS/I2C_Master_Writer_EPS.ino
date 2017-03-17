c465d720000d8297db525ef9798f00442666f9f7
AnthonyGillan <ag14125@my.bristol.ac.uk>
2017-03-17 17:53:37 +0000

    Example of send operation
    Currents, voltages and temperatures may be sent at different times

--- I2C_Master_Writer_EPS/I2C_Master_Writer_EPS.ino
+++ I2C_Master_Writer_EPS/I2C_Master_Writer_EPS.ino
@@ -12,10 +12,26 @@ float voltage2 = 39.68;
 float voltage3 = 39.69;
 float voltage4 = 39.70;
 
-float voltageArray[4] = {0};
+float current1 = 38.67;
+float current2 = 38.68;
+float current3 = 38.69;
+float current4 = 38.70;
+float current5 = 38.67;
+float current6 = 38.68;
+float current7 = 38.69;
+float current8 = 38.70;
+
+float temp1 = 37.67;
+float temp2 = 37.68;
+float temp3 = 37.69;
+
+float voltageArray  [4] = {0};
+float currentArray  [8] = {0};
+float tempArray     [3] = {0};
 
 void setup() {
-  Wire.begin(8);    // join i2c bus (address optional for master)
+  Wire.begin(8);            // join i2c bus (address optional for master)
+  Wire.setClock(400000);    // set I2C clock freq to 400kHz
   Serial.begin(9600);
 }
  
@@ -26,9 +42,24 @@ void loop() {
   voltageArray[2] = voltage3;
   voltageArray[3] = voltage4;
 
+  currentArray[0] = current1;
+  currentArray[1] = current2;
+  currentArray[2] = current3;
+  currentArray[3] = current4;
+  currentArray[4] = current5;
+  currentArray[5] = current6;
+  currentArray[6] = current7;
+  currentArray[7] = current8;
+
+  tempArray[0] = temp1;
+  tempArray[1] = temp2;
+  tempArray[2] = temp3;
+
   Wire.beginTransmission(8);  // transmit to device #8
 
-  splitFloatsIntoBytesAndSend(voltageArray, 4);
+  // splitFloatsIntoBytesAndSend(voltageArray, 4);
+  splitFloatsIntoBytesAndSend(currentArray, 8);
+  // splitFloatsIntoBytesAndSend(tempArray, 3);
 
   Wire.endTransmission();    // stop transmitting
 
@@ -36,6 +67,9 @@ void loop() {
 }
 
 void splitFloatsIntoBytesAndSend(float * values, uint8_t sizeOfArray) {
+  // create a union which will contain the float variable (f) in the same address space as four bytes (byte[4])
+  // the float can be accessed as individual bytes which can be sent over I2C then reassembled on the other side 
+  // using another union to reverse the process
   union {
     float f;
     uint8_t bytes[4];
