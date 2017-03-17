ff24a7395635a94658c7753dca5d10249836affc
AnthonyGillan <ag14125@my.bristol.ac.uk>
2017-03-17 17:48:48 +0000

    Covered a wider range of receive cases
    Now can tell how many floats are being sent and can interpret whether
    it is the array of voltage, current or temperature samples that are
    being sent.

--- I2C_Slave_Receiver_EPS/I2C_Slave_Receiver_EPS.ino
+++ I2C_Slave_Receiver_EPS/I2C_Slave_Receiver_EPS.ino
@@ -1,17 +1,13 @@
 #include <Wire.h>
 
-const int16_t sizeOfDataArray        = 4;
-// const int16_t numberOfBytesToReceive = sizeOfDataArray*4;  // each float is split into 4 bytes to be sent over I2C
-
-float receivedVoltage1 = 0;
-float receivedVoltage2 = 0;
-float receivedVoltage3 = 0;
-float receivedVoltage4 = 0;
-
-float receivedVoltageArray[sizeOfDataArray] = {0};
+int16_t sizeOfDataArray;
+float receivedVoltages [4] = {0};
+float receivedCurrents [8] = {0};
+float receivedTemps    [3] = {0};
 
 void setup() {
-  Wire.begin(8);                // join i2c bus with address #8
+  Wire.begin(8);                // join I2C bus with address #8
+  Wire.setClock(400000);        // set I2C clock freq to 400kHz
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
 }
@@ -20,30 +16,53 @@ void loop() {
   delay(100);
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
+// executes whenever data is received from master
 void receiveEvent(int numberOfBytesToReceive) {
 
-  assembleFloatsfromBytes(receivedVoltageArray, sizeOfDataArray);
+  sizeOfDataArray = (Wire.available())/4;     // the amount of bytes in a transfer /4 to give the number of floats being transferred
 
-  for (int16_t i = 0; i < sizeOfDataArray; ++i) {
-    Serial.println(receivedVoltageArray[i]);         // print the array
+  switch(sizeOfDataArray) {
+    case 4: // array of voltages
+      assembleFloatsfromBytes(receivedVoltages, sizeOfDataArray);
+
+      for (int16_t i = 0; i < sizeOfDataArray; ++i) {
+        Serial.println(receivedVoltages[i]);         // print the array
+      }
+      Serial.println("v conversion complete");
+      break;
+    case 8: // array of currents
+      assembleFloatsfromBytes(receivedCurrents, sizeOfDataArray);
+
+      for (int16_t i = 0; i < sizeOfDataArray; ++i) {
+        Serial.println(receivedCurrents[i]);         // print the array
+      }
+      Serial.println("i conversion complete");
+      break;
+    case 3: // array of temperatures
+      assembleFloatsfromBytes(receivedTemps, sizeOfDataArray);
+
+      for (int16_t i = 0; i < sizeOfDataArray; ++i) {
+        Serial.println(receivedTemps[i]);         // print the array
+      }
+      Serial.println("t conversion complete");
+      break;
   }
-
-  Serial.println("conversion complete");
 }
 
 void assembleFloatsfromBytes(float * values, int16_t sizeOfArray) {
-  union {
+  // create a union which will contain the float variable (f) in the same address space as four bytes (byte[4])
+  // the bytes are thus the four bytes of the float and can be received over I2C
+  // they are then accessed as a float
+  union {                    
     float f;
     int8_t bytes[4];
   } Union;
 
   for (int16_t i = 0; i < sizeOfArray; i++) {
-    Union.bytes[3] = Wire.read(); //0x41; // MSB
-    Union.bytes[2] = Wire.read(); //0x70;
-    Union.bytes[1] = Wire.read(); //0x00;
-    Union.bytes[0] = Wire.read(); //0x00;
+    Union.bytes[3] = Wire.read(); // MSB
+    Union.bytes[2] = Wire.read();
+    Union.bytes[1] = Wire.read();
+    Union.bytes[0] = Wire.read(); // LSB
 
     values[i] = Union.f;
   }
